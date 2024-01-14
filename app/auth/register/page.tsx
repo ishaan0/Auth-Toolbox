c7411d9d2bd0fcfa60d3bda4d8e6d25d76df2243
Muhammad Ishaan <iftekharctgbd@gmail.com>
2024-01-14 22:10:08 +0600

    feat: add register ui

---
+++ app/auth/register/page.tsx
@@ -0,0 +1,12 @@
+import { RegisterForm } from "@/components/auth/register-form";
+import React from "react";
+
+const registerPage = () => {
+  return (
+    <div>
+      <RegisterForm />
+    </div>
+  );
+};
+
+export default registerPage;
